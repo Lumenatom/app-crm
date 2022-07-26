e5c58c8b8fc8d9edcbc05682a905eae1de7312e2
Lumenatom <lumenatom@gmail.com>
2022-07-26 11:48:35 +0300

    add route to workspace

--- src/components/loginAndRegestration/login/login.js
+++ src/components/loginAndRegestration/login/login.js
@@ -10,74 +10,77 @@ import axios from "axios";
 class Login extends React.Component {
   constructor() {
     super();
-    this.state = {value: 'привет'};
+    this.state = { value: 'привет' };
     this.tryLogin = this.tryLogin.bind(this)
 
   }
 
   //Изменяем this.state.value при изменении инпута:
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
-  tryLogin(event){
+  tryLogin(event) {
 
     event.preventDefault()
-    axios.get(`http://185.94.164.62/newapi/auth`,{
+    axios.get(`http://185.94.164.62/newapi/auth`, {
 
     })
-        .then(res => {
-          console.log(res)
-        })
+      .then(res => {
+        console.log(res)
+      })
 
   }
   render() {
     return (
-        <div className={style.login}>
-          <div className={style.login__logo}>
-            <NavLink to={'/'}>
-              <img src={logo}/>
+      <div className={style.login}>
+        <div className={style.login__logo}>
+          <NavLink to={'/'}>
+            <img src={logo} />
+          </NavLink>
+        </div>
+        <div className={style.login__form}>
+          <div className={style.buttons__login__registration}>
+            <button>
+              Вход
+            </button>
+            <NavLink to={'/regestration'}>
+              Регистрация
             </NavLink>
           </div>
-          <div className={style.login__form}>
-            <div className={style.buttons__login__registration}>
-              <button>
-                Вход
-              </button>
-              <NavLink to={'/regestration'}>
-                Регистрация
-              </NavLink>
-            </div>
-            <form href="#">
-              <label className={style.inputs}>
-                <h2>
-                  Логин
-                </h2>
-                <input value={this.state.value} onChange={this.handleChange.bind(this)} />
-              </label>
-              <label className={style.inputs}>
-                <h2>
-                  Пароль
-                </h2>
-                <input type="text" name="login"/>
+          <form href="#">
+            <label className={style.inputs}>
+              <h2>
+                Логин
+              </h2>
+              <input value={this.state.value} onChange={this.handleChange.bind(this)} />
+            </label>
+            <label className={style.inputs}>
+              <h2>
+                Пароль
+              </h2>
+              <input type="text" name="login" />
 
-              </label>
-              <label className={style.checkbox}>
-                <input type="checkbox"/>
-                Запомнить
-              </label>
-              <label className={style.button_submit}>
-                <button onClick={this.tryLogin} type="submit">
+            </label>
+            <label className={style.checkbox}>
+              <input type="checkbox" />
+              Запомнить
+            </label>
+            <label className={style.button_submit}>
+              <button onClick={this.tryLogin} type="submit">
+                <NavLink to={"/workspace"}>
                   Войти
-                </button>
-              </label>
-            </form>
-            <div className={style.help_password}>
-              <button>
-                Забыли пароль?
+                </NavLink>
               </button>
-            </div>
+            </label>
+          </form>
+          <div className={style.help_password}>
+            <button>
+              Забыли пароль?
+            </button>
           </div>
         </div>
+
+      </div>
     )
   };
 }
