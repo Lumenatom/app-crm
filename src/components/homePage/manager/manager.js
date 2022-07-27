251f271ee4b68dc9e1ee933c25a5be2df2d33628
Lumenatom <lumenatom@gmail.com>
2022-07-27 13:48:01 +0300

    refact nav

--- src/components/homePage/manager/manager.js
+++ src/components/homePage/manager/manager.js
@@ -6,7 +6,7 @@ import manager from '../../../img/manager__block/manager.png';
 const Manager = () => {
   return (
     <div className={style.manager}>
-      <div>
+      <div className={style.manager__photo}>
         <img src={manager} alt='manager' />
       </div>
     </div>
