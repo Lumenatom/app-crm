e4871b4e5e740d0346f79b4faf6a94efc86201b0
Lumenatom <lumenatom@gmail.com>
2022-07-28 12:08:30 +0300

    ref dashboard

--- src/components/workspace/nav/nav.js
+++ src/components/workspace/nav/nav.js
@@ -2,17 +2,17 @@ import React, { useState } from "react";
 import burger_menu from "../../../img/nav__block/menu.png";
 import close_menu from "../../../img/nav__block/close.png";
 import { NavLink } from "react-router-dom";
-import dashboard from "../../../img/nav__block/dashboard.svg";
-import dashboard_company from "../../../img/nav__block/dashboard company.svg";
-import statistic from "../../../img/nav__block/statistika.svg";
-import offers from "../../../img/nav__block/Offers.svg";
-import income from "../../../img/nav__block/income.svg";
-import finance from "../../../img/nav__block/finance.svg";
-import form from "../../../img/nav__block/order form.svg";
-import list from "../../../img/nav__block/list.svg";
-import cards from "../../../img/nav__block/cards.svg";
-import information from "../../../img/nav__block/info.svg";
-import users from "../../../img/nav__block/users.svg";
+import { ReactComponent as Dashboard } from "../../../img/nav__block/dashboard.svg";
+import { ReactComponent as Dashboard_company } from "../../../img/nav__block/dashboard company.svg";
+import { ReactComponent as Statistic } from "../../../img/nav__block/statistika.svg";
+import { ReactComponent as Offers } from "../../../img/nav__block/Offers.svg";
+import { ReactComponent as Income } from "../../../img/nav__block/income.svg";
+import { ReactComponent as Finance } from "../../../img/nav__block/finance.svg";
+import { ReactComponent as Form } from "../../../img/nav__block/order form.svg";
+import { ReactComponent as List } from "../../../img/nav__block/list.svg";
+import { ReactComponent as Cards } from "../../../img/nav__block/cards.svg";
+import { ReactComponent as Information } from "../../../img/nav__block/info.svg";
+import { ReactComponent as Users } from "../../../img/nav__block/users.svg";
 import "./nav.css";
 import dot from "../../../img/nav__block/dot.png";
 
@@ -39,67 +39,67 @@ const Nav = () => {
       </h4>
       <div className="nav__dashboard">
         <NavLink to={'/'}>
-          <img src={dashboard} alt='dashboard' />
+          <Dashboard className="svg__nav" />
           <h2>
             Дашборд
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={dashboard_company} alt='dashboard company' />
+          <Dashboard_company className="svg__nav" />
           <h2>
             Дашборд компании
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={statistic} alt='statistic' />
+          <Statistic className="svg__nav" />
           <h2>
             Статистика
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={offers} alt='offers' />
+          <Offers className="svg__nav" />
           <h2>
             Офферы
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={income} alt='income' />
+          <Income className="svg__nav" />
           <h2>
             Расход | Доход
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={finance} alt='finance' />
+          <Finance className="svg__nav" />
           <h2>
             Финансы
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={form} alt='form' />
+          <Form className="svg__nav" />
           <h2>
             Форма заказа
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={list} alt="list" />
+          <List className="svg__nav" />
           <h2>
             Список заказов
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={cards} alt="cards" />
+          <Cards className="svg__nav" />
           <h2>
             Карты
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={information} alt="information" />
+          <Information className="svg__nav" />
           <h2>
             Информация
           </h2>
         </NavLink>
         <NavLink to={'/'}>
-          <img src={users} alt="users" />
+          <Users className="svg__nav" />
           <h2>
             Пользователи
           </h2>
